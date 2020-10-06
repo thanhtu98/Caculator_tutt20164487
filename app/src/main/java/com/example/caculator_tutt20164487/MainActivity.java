740ee67b269fb0b5d87428571cd57b89872458ce
Thanh Tú Trần <tran.thanhtu.9638@gmail.com>
2020-10-06 23:13:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/caculator_tutt20164487/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.caculator_tutt20164487;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
